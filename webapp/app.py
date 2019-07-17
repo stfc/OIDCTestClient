a27b678874242a1d1c7f9d6175e18f72e1199fd5
BranHawkes <52957061+BranHawkes@users.noreply.github.com>
2019-07-17 13:59:20 +0100

    Added Flask-pyoidc
    used for token sending and getting responses

--- webapp/app.py
+++ webapp/app.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template , request
+import datetime
+import flask
+import logging
 import inspect
+from flask import Flask, jsonify, render_template , request
+import json
+from flask_pyoidc.flask_pyoidc import OIDCAuthentication
+from flask_pyoidc.provider_configuration import ProviderConfiguration, ClientMetadata
+from flask_pyoidc.user_session import UserSession
+app = Flask(__name__)
+# See http://flask.pocoo.org/docs/0.12/config/
+app.config.update({'SERVER_NAME': 'localhost:5000',
+                   'SECRET_KEY': 'dev_key',  # make sure to change this!!
+                   'PERMANENT_SESSION_LIFETIME': datetime.timedelta(days=7).total_seconds(),
+                   'PREFERRED_URL_SCHEME': 'http',
+                   'DEBUG': True})
+ISSUER1 = 'https://provider1.example.com'
+CLIENT1 = 'client@provider1'
+PROVIDER_NAME1 = 'provider1'
+PROVIDER_CONFIG1 = ProviderConfiguration(issuer=ISSUER1,
+                                         client_metadata=ClientMetadata(CLIENT1, 'secret1'))
+auth = OIDCAuthentication({PROVIDER_NAME1: PROVIDER_CONFIG1})
+data=0
 variables=['name','phone','email']
-'''
-
-'''
 possible_variables=['name','email','phone','access_token','token_type','expires_in','refresh_token','scope']
 pv=possible_variables
 def rn(var):
@@ -31,8 +49,6 @@ def variable_list(data,possible_variables):
         except:
             nd.append(0)
     return nd
-
-
 def checkhtml(file,variables):
     import os
     path=os.path.dirname(os.path.realpath(__file__))
@@ -49,7 +65,6 @@ def checkhtml(file,variables):
     htmls=html_split
     #for i in htmls:
         #print(i)
-
     htmlss=list(htmls)
     for i in range(0,len(htmls)):
         htmlss[i]=htmls[i].split()
@@ -65,21 +80,7 @@ def checkhtml(file,variables):
                     if htmlss[i][o+1].index('}}')!=-1:
                         nv[i].append(htmlss[i][o])
             except:
-                None
-                        
-    '''               
-    for i in range(0,len(htmls)):
-        works=[]
-        for o in range(0,len(variables)):
-
-        
-            try:
-                if htmls[i].index(variables[o])!=-1:
-                    works.append(True)
-            except:
-                works=works
-
-        print(works)'''
+                None                      
     #print(nv)
     linemods=[]
     for i in nv:
@@ -100,13 +101,15 @@ def checkhtml(file,variables):
     for i in htmls:
         if i!='':
             html=html+'\n' + i
-    
     f=open(path+'\\templates\\'+file+'.html','w')
     f.write(html)
     f.close()
     return html
 
-app = Flask(__name__)
+
+@auth.error_view
+def error(error=None, error_description=None):
+    return jsonify({'error': error, 'message': error_description})
 
 @app.route('/')
 def about():
@@ -120,12 +123,23 @@ def login():
         text = request.form['button']
         processed_text = text.upper()
     return render_template('login.html')
+
+@app.route('/logout')
+@auth.oidc_logout
+def logout():
+    return "You've been successfully logged out!"
+
 @app.route("/forward/", methods=['POST'])
+@auth.oidc_auth(PROVIDER_NAME1)
+
 def logging():
     #authorisation stuff
-    print('do stuff')
-
-
+    user_session = UserSession(flask.session)
+    data = jsonify(access_token=user_session.access_token,
+                   id_token=user_session.id_token,
+                   userinfo=user_session.userinfo)
+    global parseddata = json.loads(data)
+    
 @app.route('/endpoint')
 def end():
     name='h,agf,jgaf,asgdfemjfgrymaymaymgarh,ayhdfga,hdfg,jadsf'
@@ -137,7 +151,7 @@ def end():
     refresh_token='a,hrbf,eahb,jtrgtrht,jargh,jea,jeafbr,jhebea,jrb'
     scope='a.rhe,,,,,,,,,,,,,,,frgjeh,ag,jghea,jfrgheaf,jhrg,rhghragj'
     global pv
-    data={"access_token":"MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI3",
+    """data={"access_token":"MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI3",
       "token_type":"bearer",
       "expires_in":3600,
       "refresh_token":"IwOGYzYTlmM2YxOTQ5MGE3YmNmMDFkNTVk",
@@ -145,20 +159,23 @@ def end():
       'name':'anyone',
       'email':'me@gmail.co.uk',
       'phone':'007'
-      }
+      }"""
+    global parseddata
+    data=parseddata
     cv=[]
     nd=variable_list(data,possible_variables)
     for i in range(0,len(nd)):
         if nd[i]!=0:
             cv.append(pv[i])
-    checker=nd[pv.index(rn(name)[0])]
-    
-    
-    
+    checker=nd[pv.index(rn(name)[0])] 
     checkhtml('name',cv)
     return render_template('name.html',name=checker,email=nd[pv.index(rn(email)[0])],phone=nd[pv.index(rn(phone)[0])])
 #@app.route('/hello/<name>')
 #def hello(name):
     #return render_template('name.html', name=name)
+
+
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    auth.init_app(app)
     app.run(debug=True, host='0.0.0.0')
